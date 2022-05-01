68eb04c1d716a062fdf6f83e2e67c9c6f3d15cd5
MarlenniMarTorres <91311149+MarlenniMarTorres@users.noreply.github.com>
2022-05-01 00:36:07 -0500

    se quito el menu

--- JAVA/src/Pedidos/Main.java
+++ JAVA/src/Pedidos/Main.java
@@ -167,65 +167,7 @@ public class Main {
     }
 
 
-    public  static  void menu(){
-        System.out.println("*******************************");
 
-        System.out.println("Ingresa la operacion que desea Realizar ");
-        System.out.println("***** 1. Ingresa un nuevo pedido");
-        System.out.println("***** 2. lista los Pedidos Aprovados");
-        System.out.println("***** 3. lista los clientes registrados");
-
-        Scanner sc= new Scanner(System.in);
-        Pedidos ojbpedido1= new Pedidos();
-        GestionPedidos objpedidos=new GestionPedidos();
-
-        int rpta= sc.nextInt();
-
-        try {
-            switch (rpta){
-                case 1:
-                    System.out.println(" Ingrese codigo Cliente :");
-                    ojbpedido1.setCodigoCliente(sc.nextLine());
-                    System.out.println(" Ingrese Nombres Cliente :");
-                    ojbpedido1.setNombres(sc.nextLine());
-                    System.out.println(" Ingrese Apellidos Cliente :");
-                    ojbpedido1.setApellidos(sc.nextLine());
-                    System.out.println(" Ingrese MedioContacto Cliente :");
-                    ojbpedido1.setMedioContacto(sc.nextLine());
-                    System.out.println(" Ingrese Direccion Cliente :");
-                    ojbpedido1.setDireccion(sc.nextLine());
-                    System.out.println(" Ingrese Distrito Cliente :");
-                    ojbpedido1.setDistrito(sc.nextLine());
-                    System.out.println(" Ingrese Telefono Cliente :");
-                    ojbpedido1.setTelefono(sc.nextLine());
-                    System.out.println(" Ingrese Correo Cliente :");
-                    ojbpedido1.setCorreo(sc.nextLine());
-                    System.out.println(" Ingrese CodigoPedido Cliente :");
-                    ojbpedido1.setCodigoCliente(sc.nextLine());
-                    System.out.println(" Ingrese PedidoRealizado Cliente :");
-                    ojbpedido1.setPedidosRealizado(sc.nextLine());
-                    System.out.println(" Ingrese Estado  inicial del pedido  :");
-                    ojbpedido1.setEstados(sc.nextLine());
-                    objpedidos.AgregarCliente(ojbpedido1);
-                    break;
-                case 2:
-                    objpedidos.ListarClientes();
-
-                case 3:
-                    objpedidos.ListarPedidos();
-
-                default:
-                    break;
-
-
-
-
-
-            }
-
-        } catch (Exception e){
-            System.out.println("Ingrese un numero valido"+e);
-        }
 
 
 
