6c3cdf25c193b63908d0eb9d2d4c0ab384aad441
rcayraz <ayra.zevallos@gmail.com>
2022-05-01 19:07:49 -0500

    update main

--- JAVA/src/Pedidos/Main.java
+++ JAVA/src/Pedidos/Main.java
@@ -27,6 +27,7 @@ public class Main {
 
     }
 
+
     public static void bienvenida() {
         System.out.println(); System.out.println("");
         System.out.println("===========SISTEMA DE VENTAS POR DELIVERY FASTY GO! v1.0===========");
@@ -90,8 +91,8 @@ public class Main {
         int seleccionMenu = scan.nextInt();
         switch (seleccionMenu) {
             case 1 : AgregarProductos(); break;
-            //case 2 : AgregarPedidos();break;
-           // case 3 : reporteEnviosDiarios();break;
+            case 2 : RegistroClientes();break;
+            case 3 : AsignarPedidos();break;
             case 4 : reporteClientes();break;
             case 5 : menuLogin(); break;
             default: System.out.println("Seleccionar una opción correcta");break;
@@ -112,14 +113,44 @@ public class Main {
     }
 
 
+    public static  void RegistroClientes(){
+        GestionPedidos objGestionPedidos= new GestionPedidos();
+        objGestionPedidos.RegistrarCliente("60267465","ROBERTO CARLOS","AYRA ZEVALLOS","FACEBOOK","AV LA MARINA 12","SAN MIGUEL","96545856","ayra.zevallos@mail.com");
+        objGestionPedidos.RegistrarCliente("26656456","JUAN CARLOS","RAMIRES VARGAS","INSTRAGRAM","AV LA PAZ 1523","SAN MIGUEL","985656232","ramires.v@mail.com");
+        objGestionPedidos.RegistrarCliente("546545646","GEANS","PEREZ GONZALES","WEB","AV JORGE CHAVEZ 564","JESUS MARIA","3232326","p.gonsales1@mail.com");
+        objGestionPedidos.RegistrarCliente("54556323","MANUEL","ROMERO PILCO","LLAMADA TELEFONICA","AV LOS SAUCES 88 ","COMAS","46565656","elunico0001.sls@mail.com");
+        objGestionPedidos.RegistrarCliente("2645789","JORGE","FALCON PEREZ","FACEBOOK","AV LOS POSTES 898","SJL","96545856","f.pss@mail.com");
+        objGestionPedidos.RegistrarCliente("23145685","JOSE","HERRERA CASTILLO","CHAT APLICATIVO","JR VIRGEN MARIA 8989","RIMAC","656565989","herrrera.castillo@mail.com");
+        objGestionPedidos.RegistrarCliente("6225874","MARIA","MANUEL RAMIREZ","INSTAGRAM","JR EL DIABLO 666","SJM","98989895","manuel.9898@mail.com");
+        objGestionPedidos.RegistrarCliente("62597465","MIGUEL ","RODRIGUES BALTA","LLAMADA","JR JORGE MUÑOZ 6565","RIMAC","98756565","balta.989898@mail.com");
+        objGestionPedidos.RegistrarCliente("26746552","SUSANA","QUIÑONES PEÑA","FACEBOOK","JR PEDRO CASTILLO 6565","CERCADO DE LIMA","96545856","q.suana@mail.com");
+        objGestionPedidos.RegistrarCliente("264848565","PEDRO","CABELLERO FALCON","FACEBOOK","JR ALAN GARCIA 9898","RIMAC","5656556","caballero.falcon@mail.com");
+        System.out.println(objGestionPedidos.toString());
+
+
+    }
+
+    public  static  void AsignarPedidos(){
+        GestionPedidos objGestionPedidos= new GestionPedidos();
+        objGestionPedidos.AsignarPedidoCliente("60267465","PED0001","PC GAMER","ingresado");
+        objGestionPedidos.AsignarPedidoCliente("26656456","PED0002","PC ESCRITORIO","Aceptado");
+        objGestionPedidos.AsignarPedidoCliente("54556323","PED0003","PANTALONES TALLA 30","Aceptado");
+        objGestionPedidos.AsignarPedidoCliente("60267465","PED0004","SILLA GAMER","Aceptado");
+        objGestionPedidos.AsignarPedidoCliente("26746552","PED005","ESCRITORIO DE OFICINA","Rechazado");
+        System.out.println(objGestionPedidos.toString());
+    }
+
 
     public static void AgregarProductos() {
+
+
         ArtefactosElec p1 = new ArtefactosElec("AECOMAG4QH", "Cocina a gas Mabe 4hornillas", 699.00);
         ArtefactosElec p2 = new ArtefactosElec("AETVLG55FHD", "TV LG de 55pulg. UHD 4K Smart", 1900.00);
         ropaYcalzado p3 = new ropaYcalzado("RCPAJDAS", "Pantalon jean para dama, talla small", 150.00);
         ropaYcalzado p4 = new ropaYcalzado("RCCAJHOcP", "Casaca jean hombre con peluche", 230.00);
         Muebles p5 = new Muebles("MUJCO6MACA", "Juego de comedor 6 sillas, en Caoba", 1600.00);
         Muebles p6 = new Muebles("MUMTV65ORG", "Mesa de TV 65pulg. Con organizador", 750.00);
+
 
         Empresa empresa = new Empresa();
         empresa.registrar(p1);
--- JAVA/src/Pedidos/Test.java
+++ JAVA/src/Pedidos/Test.java
@@ -6,23 +6,12 @@ public class Test {
 
 
 
-        GestionPedidos objGestionPedidos= new GestionPedidos();
-        objGestionPedidos.RegistrarCliente("60267465","ROBERTO CARLOS","AYRA ZEVALLOS","FACEBOOK","AV LA MARINA 12","SAN MIGUEL","96545856","ayra.zevallos@mail.com");
-        objGestionPedidos.RegistrarCliente("26656456","JUAN CARLOS","RAMIRES VARGAS","INSTRAGRAM","AV LA PAZ 1523","SAN MIGUEL","985656232","ramires.v@mail.com");
-        objGestionPedidos.RegistrarCliente("546545646","GEANS","PEREZ GONZALES","WEB","AV JORGE CHAVEZ 564","JESUS MARIA","3232326","p.gonsales1@mail.com");
-        objGestionPedidos.RegistrarCliente("54556323","MANUEL","ROMERO PILCO","LLAMADA TELEFONICA","AV LOS SAUCES 88 ","COMAS","46565656","elunico0001.sls@mail.com");
-        objGestionPedidos.RegistrarCliente("2645789","JORGE","FALCON PEREZ","FACEBOOK","AV LOS POSTES 898","SJL","96545856","f.pss@mail.com");
-        objGestionPedidos.RegistrarCliente("23145685","JOSE","HERRERA CASTILLO","CHAT APLICATIVO","JR VIRGEN MARIA 8989","RIMAC","656565989","herrrera.castillo@mail.com");
-        objGestionPedidos.RegistrarCliente("6225874","MARIA","MANUEL RAMIREZ","INSTAGRAM","JR EL DIABLO 666","SJM","98989895","manuel.9898@mail.com");
-        objGestionPedidos.RegistrarCliente("62597465","MIGUEL ","RODRIGUES BALTA","LLAMADA","JR JORGE MUÑOZ 6565","RIMAC","98756565","balta.989898@mail.com");
-        objGestionPedidos.RegistrarCliente("26746552","SUSANA","QUIÑONES PEÑA","FACEBOOK","JR PEDRO CASTILLO 6565","CERCADO DE LIMA","96545856","q.suana@mail.com");
-        objGestionPedidos.RegistrarCliente("264848565","PEDRO","CABELLERO FALCON","FACEBOOK","JR ALAN GARCIA 9898","RIMAC","5656556","caballero.falcon@mail.com");
 
 
 
-        objGestionPedidos.AsignarPedidoCliente("60267465","ped000160267465","PC GAMER","ingresado");
+       // objGestionPedidos.AsignarPedidoCliente("60267465","ped000160267465","PC GAMER","ingresado");
 
-        System.out.println(objGestionPedidos.toString());
+        //System.out.println(objGestionPedidos.toString());
 
 
 
